c8138ba10895b236c9a78ccabac30522bad227df
TiimmyTurner <shushlyakovd@mail.ru>
2022-02-10 18:42:20 +0300

    Input
    InputService, GameBootstrapper, Hero Movement

---
+++ Architecture/Assets/CodeBase/Hero/HeroAnimator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace CodeBase.Hero
+{
+    public class HeroAnimator: MonoBehaviour
+    {
+        private const string Speed = "Speed";
+        
+        public CharacterController CharacterController;
+        public Animator Animator;
+
+        private void Update()
+        {
+            Animator.SetFloat(Speed, CharacterController.velocity.magnitude, 0.1f, Time.deltaTime);
+        }
+    }
+}
---
+++ Architecture/Assets/CodeBase/Infrastructure/GameBootstrapper.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure
+{
+    public class GameBootstrapper : MonoBehaviour
+    {
+        private Game _game;
+        private void Awake()
+        {
+            _game = new Game();
+            
+            DontDestroyOnLoad(this);
+        }
+    }
+}
